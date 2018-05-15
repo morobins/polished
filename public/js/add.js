4e9a236055b9b3f4c3609fc930dc12312e24a055
Monique Robinson <monique.robinson2@gmail.com>
2018-05-14 23:23:07 -0400

    add.js isn't working

--- public/js/add.js
+++ public/js/add.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
   }
 
   // Getting jQuery references to the post body, title, form, and category select
-  var addForm = $("#add");
+  var addForm = $("#addsub");
   var brandInput = $("#brand");
   var productInput = $("#productName");
   var colorInput = $("#color");
@@ -23,70 +23,82 @@ $(document).ready(function() {
   // Giving the postCategorySelect a default value
   postCategorySelect.val("Nails");
   // Adding an event listener for when the form is submitted
-  $(addForm).on("submit", function handleFormSubmit(event) {
+  $(addForm).on("submit", function (event) {
     event.preventDefault();
     // Won't submit the post if we are missing a category
-    if (!postCategorySelect.val().trim()) {
-      return;
-    }
+    // if (!postCategorySelect.val().trim()) {
+    //   return;
+    // }
     // Constructing a newPost object to hand to the database
     var newPost = {
       brand: brandInput.val().trim(),
-      product: productInput.val().trim(),
+      product_name: productInput.val().trim(),
       color: colorInput.val().trim(),
-      comment: commentInput.val().trim(),
-photo: photoInput.val(),
+      notes: commentInput.val().trim(),
+      photo: photoInput.val(),
       category: postCategorySelect.val()
     };
 
     console.log(newPost);
 
-    // If we're updating a post run updatePost to update a post
-    // Otherwise run submitPost to create a whole new post
-    if (updating) {
-      newPost.id = postId;
-      updatePost(newPost);
-    }
-    else {
-      submitPost(newPost);
-    }
-  });
+    $.post("/api/products", newPost)
+        .done(function (data) {
+          console.log(data);
+          // console.log("name is: " + data.name)
+          // $("#resultsModal").modal('toggle');
+          // $("#bestFriend").text(data.name);
+          // $('#friendImage').attr("src", data.photo);
 
-  // Submits a new post and brings user to blog page upon completion
-  function submitPost(Post) {
-    console.log('subbmitting!');
-    $.post("/api/products/", Post, function() {
-      window.location.href = "/collection";
-    });
-  }
+        });
+    
 
-  // Gets post data for a post if we're editing
-  function getPostData(id) {
-    $.get("/api/products/" + id, function(data) {
-      if (data) {
-        // If this post exists, prefill our add forms with its data
-        brandInput.val(data.brand);
-        productInput.val(data.productName);
-        colorInput.val(data.color);
-        commentInput.val(data.comment);
-        photoInput.val(data.photo);
-        postCategorySelect.val(data.category);
-        // If we have a post with this id, set a flag for us to know to update the post
-        // when we hit submit
-        updating = true;
-      }
-    });
-  }
+  //   // If we're updating a post run updatePost to update a post
+  //   // Otherwise run submitPost to create a whole new post
+  //   if (updating) {
+  //     newPost.id = postId;
+  //     updatePost(newPost);
+  //   }
+  //   else {
+  //     submitPost(newPost);
+  //   }
+  // });
 
-  // Update a given post, bring user to the blog page when done
-  function updatePost(post) {
-    $.ajax({
-      method: "PUT",
-      url: "/api/products",
-      data: post
-    })
-      .then(function() {
-        window.location.href = "/collection";
-      });
-  }
+  // // Submits a new post and brings user to blog page upon completion
+  // function submitPost(newPost) {
+  //   console.log('submitting!');
+  //   $.post("/api/products/", newPost, function() {
+  //     window.location.href = "./../collection.html";
+  //   });
+  // }
+
+  // // Gets post data for a post if we're editing
+  // function getPostData(id) {
+  //   $.get("/api/products/" + id, function(data) {
+  //     if (data) {
+  //       // If this post exists, prefill our add forms with its data
+  //       brandInput.val(data.brand);
+  //       productInput.val(data.productName);
+  //       colorInput.val(data.color);
+  //       commentInput.val(data.comment);
+  //       photoInput.val(data.photo);
+  //       postCategorySelect.val(data.category);
+  //       // If we have a post with this id, set a flag for us to know to update the post
+  //       // when we hit submit
+  //       updating = true;
+  //     }
+  //   });
+  // }
+
+  // // Update a given post, bring user to the blog page when done
+  // function updatePost(newPost) {
+  //   $.ajax({
+  //     method: "PUT",
+  //     url: "/api/products",
+  //     data: newPost
+  //   })
+  //     .then(function() {
+  //       window.location.href = "./../collection.html";
+  //     });
+  // }
+});
 });
