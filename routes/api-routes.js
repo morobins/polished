bca694d5250b52b07806f5f600385cc194a7fd2b
Monique Robinson <monique.robinson2@gmail.com>
2018-05-21 21:51:19 -0400

    heroku

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -12,9 +12,9 @@ var db = require("../models");
 var passport = require("../config/passport");
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
+  CLOUDINARY_NAME: process.env.CLOUDINARY_NAME,
+  CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET
 });
 
 // Routes
