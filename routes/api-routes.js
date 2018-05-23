31203a2149264c629f3f364c7629c00adc1582e1
Monique Robinson <monique.robinson2@gmail.com>
2018-05-23 18:17:28 -0400

    fixing cloudianry

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -11,12 +11,13 @@ require('dotenv').config();
 var db = require("../models");
 var passport = require("../config/passport");
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
-});
-
+if (!process.env.CLOUDINARY_URL) {
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+  });  
+}
 // Routes
 // =============================================================
 module.exports = function (app) {
